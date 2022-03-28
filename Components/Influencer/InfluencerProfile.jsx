3c00702717073a903954e069cf2872b2d25becdf
Elad Cohen <83339968+EladC921@users.noreply.github.com>
2022-03-28 14:27:05 +0300

    added plain files

---
+++ Components/Influencer/InfluencerProfile.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const InfluencerProfile = () => {
+  return (
+    <View>
+      <Text>InfluencerProfile</Text>
+    </View>
+  );
+};
+
+export default InfluencerProfile;
+
+const styles = StyleSheet.create({});
