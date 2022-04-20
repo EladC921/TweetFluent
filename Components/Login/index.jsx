eb51e5c38a3d9eb5c459b88f81447d96c28b99c9
Chen <74059786+Dodchen@users.noreply.github.com>
2022-04-20 14:31:29 +0300

    register modal and login finish

--- Components/Login/index.jsx
+++ Components/Login/index.jsx
@@ -1,47 +1,64 @@
-import { StyleSheet, Text, View, TouchableOpacity,TextInput,
+import {
+  StyleSheet, Text, View, TouchableOpacity, TextInput,
   Button,
-  TouchableHighlight,
   Image,
-  Alert } from "react-native";
-import React from "react";
-
+  Alert
+} from "react-native";
+import React,{useState} from "react";
+import root from "../../styles/root.json";
+import logo from "../../assets/logo.png";
+import Register from "./Register";
 
 const Login = ({ navigation }) => {
+  const [uname, setUname] = useState(null);
+  const [pass, setPass] = useState(null);
   return (
     <View style={styles.container}>
-    <View style={styles.inputContainer}>
-   
-      <TextInput style={styles.inputs}
-          placeholder="Email"
-          keyboardType="email-address"
-          underlineColorAndroid='transparent'/>
+      <View style={styles.header}>
+      <View style={styles.logoContainer}>
+          <Image source={logo} style={styles.logo} />
+        </View>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.inputContainer}>
+
+          <TextInput style={styles.inputs}
+            placeholder={"Email"}
+            keyboardType="email-address"         
+            onChangeText={username=>setUname(username)}
+            underlineColorAndroid='transparent' />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.inputs}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={pword=>setPass(pword)}
+            underlineColorAndroid='transparent' />
+        </View>
+
+      
+
+        <TouchableOpacity style={[styles.buttonContainer, styles.loginButton]}>
+          <Text style={styles.loginText}
+            onPress={() => {
+              navigation.navigate("Main");
+            }}
+          >Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.buttonContainer, styles.loginButton]}>
+          <Register/>
+         
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.restoreButtonContainer}>
+          <Text>Forgot Password?</Text>
+        </TouchableOpacity>
+      </View>
+
+
+
     </View>
-    
-    <View style={styles.inputContainer}>
-      <TextInput style={styles.inputs}
-          placeholder="Password"
-          secureTextEntry={true}
-          underlineColorAndroid='transparent'/>
-    </View>
- 
-    <TouchableOpacity style={styles.restoreButtonContainer}>
-        <Text>Forgot Password?</Text>
-    </TouchableOpacity>
-
-    <TouchableOpacity style={[styles.buttonContainer, styles.loginButton]}>
-      <Text  style={styles.loginText}
-      onPress={() => {
-        navigation.navigate("Main");
-      }}
-      >Login</Text>
-    </TouchableOpacity>
-
-    <TouchableOpacity style={[styles.buttonContainer, styles.loginButton]}>
-        <Text  style={styles.loginText}>Register</Text>
-    </TouchableOpacity>
-
-  
-  </View>
   );
 };
 
@@ -52,66 +69,86 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#B0E0E6',
+    backgroundColor: root.bg,
   },
   inputContainer: {
-      borderBottomColor: '#F5FCFF',
-      backgroundColor: '#FFFFFF',
-      borderRadius:30,
-      borderBottomWidth: 1,
-      width:250,
-      height:45,
-      marginBottom:15,
-      flexDirection: 'row',
-      alignItems:'center'
+    marginTop:10,
+    borderBottomColor: root.light,
+    backgroundColor: root.light,
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 15,
+    flexDirection: 'row',
+    alignItems: 'center'
   },
-  inputs:{
-      height:45,
-      marginLeft:16,
-      borderBottomColor: '#FFFFFF',
-      flex:1,
+
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: root.secondary,
+    flex: 1,
   },
-  icon:{
-    width:30,
-    height:30,
+  header: {
+    flex: 2,
+    justifyContent:'center',
+    backgroundColor:root.bg
   },
-  inputIcon:{
-    marginLeft:15,
-    justifyContent: 'center'
+  body: {
+    backgroundColor:root.tertiary,
+    flex: 4,
+    width:'100%',
+    borderTopRightRadius:20,
+    borderTopLeftRadius:20,
+    alignItems: "center",
+    justifyContent:'center',
+    shadowOffset: { width: 0, height: 0 },
+    shadowColor: "black",
+    shadowOpacity: 0.1,
+    shadowRadius: 20,
+  },
+  logoContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginBottom:20,
+  },
+
+  logo: {   
+    resizeMode:'contain',
+    height:'50%',
   },
   buttonContainer: {
-    height:45,
+    marginTop:15,
+    height: '10%',
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom:20,
-    width:170,
-    borderRadius:30,
+    marginBottom: 20,
+    width: '50%',
+    borderRadius: 30,
   },
+
   loginButton: {
-    backgroundColor: '#3498db',
+    backgroundColor: root.twitter,
   },
-  fabookButton: {
-    backgroundColor: "#3b5998",
-  },
-  googleButton: {
-    backgroundColor: "#ff0000",
-  },
+
   loginText: {
     color: 'white',
   },
-  restoreButtonContainer:{
-    width:250,
-    marginBottom:15,
+  restoreButtonContainer: {
+    width: 250,
+    marginBottom: 15,
     alignItems: 'center'
   },
-  socialButtonContent:{
+  socialButtonContent: {
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center', 
+    alignItems: 'center',
   },
-  socialIcon:{
+  socialIcon: {
     color: "#FFFFFF",
-    marginRight:5
+    marginRight: 5
   }
 });
